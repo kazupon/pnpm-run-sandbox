ae03c7ced369bce19469c6d53539c35d5dc238eb
kazuya kawaguchi <kawakazu80@gmail.com>
2021-10-11 18:01:40 +0900

    commit first

---
+++ packages/package1/scripts/build.mjs
@@ -0,0 +1,5 @@
+console.log('package1 building ...')
+
+setTimeout(() => {
+  console.log('... done package1 build!')
+}, 1000)
---
+++ packages/package2/scripts/build.mjs
@@ -0,0 +1,5 @@
+console.log('package2 building ...')
+
+setTimeout(() => {
+  console.log('... done package2 build!')
+}, 3000)
